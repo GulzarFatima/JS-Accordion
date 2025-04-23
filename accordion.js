3de301c647950c558210e51ccc01a84f86f72348
Gulzar Fatima <gul.aka.alukebab@gmail.com>
2025-04-23 00:45:36 -0400

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,1 +1,22 @@
+function accordion_toggle(sectionId) {
+  var allSections = document.querySelectorAll('.accordion-section');
+  var section = document.getElementById(sectionId);
 
+  allSections.forEach((s) => {
+    if (s.id !== sectionId) {
+      s.style.display = "none";
+    }
+  });
+
+  if (section.style.display === "block") {
+    section.style.display = "none";
+    console.log(`Closed: ${sectionId}`);
+    
+  } else 
+  {
+    
+    section.style.display = "block";
+    console.log(`Opened: ${sectionId}`);
+    
+  }
+}
